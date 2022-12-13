7880ff198b5cf6d611a2c55d1a3d966e8b07177c
Pedro Reis <phbarcelosreis66@gmail.com>
2022-12-13 11:16:34 -0300

    feat: starting develop first CRUD

---
+++ src/routes/router.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/categories");
+router.post("/categories");
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import router from "./routes/router";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);

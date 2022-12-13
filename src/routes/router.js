93bf9c60053b8ce8d9464529fea926ad04e38ce4
Pedro Reis <phbarcelosreis66@gmail.com>
2022-12-13 14:30:59 -0300

    feat: add routes for rentals

--- src/routes/router.js
+++ src/routes/router.js
@@ -11,13 +11,16 @@ const router = Router();
 router.get("/categories", categoriesGet);
 router.post("/categories", checkCategoriesPost, categoriesPost);
 
-router.get("/games", getGameCheck, gameGet)
-router.post("/games", gamePostValidate ,gamePost)
+router.get("/games", getGameCheck, gameGet);
+router.post("/games", gamePostValidate ,gamePost);
 
 router.get("/customers", customersGet);
 router.get("/customers:id", customersCheckID , customersGetID);
 router.post("/customers", customerPostValidate, customersPost);
-router.put("/customers/:id", customerPostValidate ,customerUpdate)
+router.put("/customers/:id", customerPostValidate ,customerUpdate);
 
+router.get("/rentals");
+router.post("/rentals/:id/return");
+router.delete("/rentals/:id")
 
 export default router;
